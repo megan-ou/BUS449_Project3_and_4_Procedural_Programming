a670341b58a95ae2ecd325fe7e0032689cd16cfc
meganou <ou27@up.edu>
2025-10-05 22:41:08 -0700

    MO - importing new tests

--- test_queues.py
+++ test_queues.py
@@ -1,17 +1,38 @@
 from unittest import TestCase
 from unittest import main
 
+
 import math
 
 import queues as q
 
-
 class Test_queues(TestCase):
+    lamda = 0.0
+    mu = 0.0
+    c = 1.0
 
     def setUp(self):
         self.lamda = 20.0
         self.mu = 25.0
         self.c = 1.0
+
+    # def tearDown(self):
+    #     result = self.defaultTestResult()  # These two methods have no side effects
+    #     print("printing captured result")
+    #     print(result)
+    #
+    #     self._feedErrorsToResult(result, self._outcome.errors)
+    #
+    #     error = self.list2reason(result.errors)
+    #     failure = self.list2reason(result.failures)
+    #     ok = not error and not failure
+    #
+    #     # Demo:   report short info immediately (not important)
+    #     if not ok:
+    #         typ, text = ('ERROR', error) if error else ('FAIL', failure)
+    #         msg = [x for x in text.split('\n')[1:] if not x.startswith(' ')][0]
+    #         print("\n%s: %s\n     %s" % (typ, self.id(), msg))
+
 
     def test_is_valid(self):
         lamda = self.lamda
@@ -267,11 +288,258 @@ class Test_queues(TestCase):
         self.assertAlmostEqual(46.8439, q.calc_lq_mmc((40, 58), 25, 4), 4)
 
 
+
+
+    def test_calc_bk_mmc(self):
+        k = 2
+        self.assertAlmostEqual(1.0, q.calc_bk_mmc(0, (5, 10, 5), 25, 1))
+        self.assertAlmostEqual(0.8, q.calc_bk_mmc(1, (5, 10, 5), 25, 1))
+        self.assertAlmostEqual(0.4, q.calc_bk_mmc(2, (5, 10, 5), 25, 1))
+        self.assertAlmostEqual(0.2, q.calc_bk_mmc(3, (5, 10, 5), 25, 1))
+
+        # test with non tuple lamda
+        self.assertAlmostEqual(1.0, q.calc_bk_mmc(0, 20, 25, 1))
+        self.assertAlmostEqual(0.2, q.calc_bk_mmc(1, 20, 25, 1))
+        self.assertTrue(math.isnan(q.calc_bk_mmc(2, 20, 25, 1)))
+
+        # test with single value tuple lamda
+        self.assertAlmostEqual(1.0, q.calc_bk_mmc(0, (20,), 25, 1))
+        self.assertAlmostEqual(0.2, q.calc_bk_mmc(1, (20,), 25, 1))
+        self.assertTrue(math.isnan(q.calc_bk_mmc(2, (20,), 25, 1)))
+
+        self.assertTrue(math.isnan(q.calc_bk_mmc(-k, 20, 25, 1)))
+        self.assertTrue(math.isnan(q.calc_bk_mmc(k, (20, -4), 25, 1)))
+        self.assertTrue(math.isnan(q.calc_bk_mmc(k, (-20, 4), 25, 1)))
+        self.assertTrue(math.isnan(q.calc_bk_mmc(1, -20, 25, 1)))
+        self.assertTrue(math.isnan(q.calc_bk_mmc(1, 20, -25, 1)))
+        self.assertTrue(math.isnan(q.calc_bk_mmc(1, 20, 25, -1)))
+        self.assertTrue(math.isnan(q.calc_bk_mmc(1, 20, -25, 1)))
+
+
+    def test_calc_wqk_mmc(self):
+        k = 2
+        self.assertAlmostEqual(0.04, q.calc_wqk_mmc(1, (5, 10, 5), 25, 1))
+        self.assertAlmostEqual(0.1, q.calc_wqk_mmc(2, (5, 10, 5), 25, 1))
+        self.assertAlmostEqual(0.4, q.calc_wqk_mmc(3, (5, 10, 5), 25, 1))
+
+        self.assertAlmostEqual(0.16, q.calc_wqk_mmc(1, (20,), 25, 1))
+        self.assertTrue(math.isnan(q.calc_wqk_mmc(2, (20,), 25, 1)))
+
+        self.assertAlmostEqual(0.16, q.calc_wqk_mmc(1, 20, 25, 1))
+        self.assertTrue(math.isnan(q.calc_wqk_mmc(2, 20, 25, 1)))
+
+        self.assertTrue(math.isnan(q.calc_wqk_mmc(-k, 20, 25, 1)))
+        self.assertTrue(math.isnan(q.calc_wqk_mmc(0, 20, 25, 1)))
+        self.assertTrue(math.isnan(q.calc_wqk_mmc(k, (20, -4), 25, 1)))
+        self.assertTrue(math.isnan(q.calc_wqk_mmc(k, (-20, 4), 25, 1)))
+        self.assertTrue(math.isnan(q.calc_wqk_mmc(1, -20, 25, 1)))
+        self.assertTrue(math.isnan(q.calc_wqk_mmc(1, 20, -25, 1)))
+        self.assertTrue(math.isnan(q.calc_wqk_mmc(1, 20, 25, -1)))
+        self.assertTrue(math.isnan(q.calc_wqk_mmc(1, 20, -25, 1)))
+
+    def test_calc_lqk_mmc(self):
+        k = 3
+        lamda = (10,15,20)
+        wqk = q.calc_wqk_mmc(k, lamda, self.mu, 2)
+
+        # validity checks
+        self.assertTrue(math.isnan(q.calc_lqk_mmc(0, lamda, wqk)))
+        self.assertTrue(math.isnan(q.calc_lqk_mmc(-k, lamda, wqk)))
+        self.assertTrue(math.isnan(q.calc_lqk_mmc(k, (10,15), wqk)))
+        self.assertTrue(math.isnan(q.calc_lqk_mmc(2, (10,), wqk)))
+        self.assertTrue(math.isnan(q.calc_lqk_mmc(k, 0, wqk)))
+
+        # test with non-numeric arguments
+        parms = [["one", 20, 25],
+                 [1, "twenty", 25],
+                 [1, 20, "twenty-five"],
+                 ["one", "twenty", 25],
+                 ["one", 20, "twenty-five"],
+                 [1, "twenty", "twenty-five"],
+                 ["one", "twenty", "twenty-five"]
+        ]
+
+        for k, lamda, wqk in parms:
+            with self.subTest(k=k, lamda=lamda, wkq=wqk):
+                self.assertTrue(math.isnan(q.calc_lqk_mmc(k, lamda, wqk)))
+
+        # test with valid arguments
+        parms = [[1, (5, 10, 15), 35],
+                 [2, (5, 10, 15), 35],
+                 [3, (5, 10, 15), 35],
+                 [1, (5, 10, 15), 100],
+                 [2, (5, 10, 15), 100],
+                 [3, (5, 10, 15), 100]
+        ]
+
+        for k, lamda, wqk in parms:
+            with self.subTest(k=k, lamda=lamda, wkq=wqk):
+                self.assertAlmostEqual(lamda[k-1] * wqk, q.calc_lqk_mmc(k, lamda, wqk))
+
+
+    def test_use_littles_law(self):
+        lamda = 1
+        mu = 2
+        c = 3
+
+        # using default values test default values for lamda, mu, and c (see setUp)
+
+        # test all variations with a single server
+        expected = { 'lq' : 3.2,
+                     'l' : 4.0,
+                     'wq' : 0.16,
+                     'w' : 0.2,
+                     'r' : 0.8,
+                     'ro' : 0.8
+                     }
+
+        # test invalid call, no lq specified
+        self.assertEqual(None,q.use_littles_law(self.lamda, self.mu, self.c))
+
+        # test invalid queue
+        self.assertTrue(math.isnan(q.use_littles_law(0, 25, 1, lq=math.nan)))
+        self.assertTrue(math.isnan(q.use_littles_law(20, 0, 1, lq=math.nan)))
+        self.assertTrue(math.isnan(q.use_littles_law(20, 25, 0, lq=math.nan)))
+        self.assertTrue(math.isnan(q.use_littles_law(0, 0, 1, lq=math.nan)))
+        self.assertTrue(math.isnan(q.use_littles_law(0, 25, 0, lq=math.nan)))
+        self.assertTrue(math.isnan(q.use_littles_law(20, 0, 0, lq=math.nan)))
+        self.assertTrue(math.isnan(q.use_littles_law(0, 0, 0, lq=math.nan)))
+
+        self.assertTrue(math.isnan(q.use_littles_law(-lamda, mu, c, lq=math.nan)))
+        self.assertTrue(math.isnan(q.use_littles_law(lamda, -mu, c, lq=math.nan)))
+        self.assertTrue(math.isnan(q.use_littles_law(lamda, mu, -c, lq=math.nan)))
+        self.assertTrue(math.isnan(q.use_littles_law(-lamda, -mu, c, lq=math.nan)))
+        self.assertTrue(math.isnan(q.use_littles_law(-lamda, mu, -c, lq=math.nan)))
+        self.assertTrue(math.isnan(q.use_littles_law(lamda, -mu, -c, lq=math.nan)))
+        self.assertTrue(math.isnan(q.use_littles_law(-lamda, -mu, -c, lq=math.nan)))
+
+
+        # test infeasible queue
+        self.infLamda = 50
+        self.infMu = 10
+        self.infC = 1
+        self.assertTrue(math.isinf(q.use_littles_law(self.infLamda, self.infMu, self.infC, lq=math.inf)))
+        self.assertTrue(math.isinf(q.use_littles_law(self.infMu, self.infMu, 1, lq=math.inf)))
+        self.assertTrue(math.isinf(q.use_littles_law(self.infLamda, self.infLamda, self.infC, lq=math.inf)))
+        self.assertTrue(math.isinf(q.use_littles_law(2 * self.lamda, self.infMu, self.infC, lq=math.inf)))
+        self.assertTrue(math.isinf(q.use_littles_law((5, 10, 15), 5, self.infC, lq=math.inf)))
+
+        actual = q.use_littles_law(self.lamda, self.mu, self.c, lq=3.2)
+
+        for k in expected.keys():
+            with self.subTest(metric=k):
+                self.assertAlmostEqual(expected[k], actual[k], 5)
+
+        actual = q.use_littles_law(self.lamda, self.mu, self.c, l=4.0)
+
+        for k in expected.keys():
+            with self.subTest(metric=k):
+                self.assertAlmostEqual(expected[k], actual[k], 5)
+
+        actual = q.use_littles_law(self.lamda, self.mu, self.c, wq=0.16)
+
+        for k in expected.keys():
+            with self.subTest(metric=k):
+                self.assertAlmostEqual(expected[k], actual[k], 5)
+
+        actual = q.use_littles_law(self.lamda, self.mu, self.c, w=0.2)
+
+
+        for k in expected.keys():
+            with self.subTest(metric=k):
+                self.assertAlmostEqual(expected[k], actual[k], 5)
+
+        # now test with multiple classes of service
+        self.lamda = (5, 20, 30)
+        self.c = 3
+
+        expected = { 'lq' : 1.49094,
+                     'l' : 3.690940,
+                     'wq' : 0.027108,
+                     'w' : 0.067108,
+                     'r' : 2.2,
+                     'ro' : 0.733333
+                     }
+        expected_qk = {'wqk': [0.007745126563863838, 0.011617689845795755, 0.04066191446028516],
+                       'lqk': [0.03872563281931919, 0.2323537969159151, 1.2198574338085548]}
+
+        actual = q.use_littles_law(self.lamda, self.mu, self.c, lq=1.49094)
+
+        #print(actual)
+
+        for k in expected.keys():
+            with self.subTest(metric=k):
+                self.assertAlmostEqual(expected[k], round(actual[k],6), places=6)
+
+        for k in expected_qk.keys():
+            with self.subTest(metric=k):
+                qkmetric = actual[k]
+                rndact = [round(x, 6) for x in qkmetric]
+                rndexp = [round(x, 6) for x in expected_qk[k]]
+                self.assertListEqual(rndexp, rndact)
+
+        # now test with a single class of service
+        self.lamda = 55
+        self.c = 3
+
+        expected = { 'lq' : 1.49094,
+                     'l' : 3.69094,
+                     'wq' : 0.02711,
+                     'w' : 0.06711,
+                     'r' : 2.2,
+                     'ro' : 0.73333
+                     }
+        expected_qk = {'wqk': [0.0077451, 0.0116177, 0.0406619],
+                       'lqk': [0.0387256, 0.2323538, 1.2198574]}
+
+        actual = q.use_littles_law(self.lamda, self.mu, self.c, lq=1.49094)
+
+        #print(actual)
+
+        for k in expected.keys():
+            with self.subTest(metric=k):
+                self.assertAlmostEqual(expected[k], actual[k], places=5)
+
+        for k in expected_qk.keys():
+            with self.subTest(metric=k):
+                self.assertFalse(k in actual)
+
+        # now test with a single class of service as a tuple
+        self.lamda = (55,)
+        self.c = 3
+
+        expected = { 'lq' : 1.49094,
+                     'l' : 3.69094,
+                     'wq' : 0.02711,
+                     'w' : 0.06711,
+                     'r' : 2.2,
+                     'ro' : 0.73333
+                     }
+        expected_qk = {'wqk': [0.0077451, 0.0116177, 0.0406619],
+                       'lqk': [0.0387256, 0.2323538, 1.2198574]
+                       }
+
+        actual = q.use_littles_law(self.lamda, self.mu, self.c, lq=1.49094)
+
+        #print(actual)
+
+        for k in expected.keys():
+            with self.subTest(metric=k):
+                self.assertAlmostEqual(expected[k], actual[k], 5)
+
+        # for k in expected_qk.keys():
+        #     with self.subTest(metric=k):
+        #         self.assertFalse(k in actual)
+
+        # verify that no qk statistics were produced
+        self.assertFalse('lqk' in actual)
+        self.assertFalse('wqk' in actual)
+
+
 # suite = unittest.TestSuite([Test_queues()])
 
 if __name__ == '__main__':
     rslt = main(verbosity=2, exit=False)
-    #Qprint(rslt)
 
     print('done')
 
